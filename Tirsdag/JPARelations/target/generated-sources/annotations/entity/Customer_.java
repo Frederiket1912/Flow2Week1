13d57c7ff0533d2dd1b8ad6ddf5a9780dad80323
Frederiket1912 <44972163+Frederiket1912@users.noreply.github.com>
2019-09-17 08:35:56 +0200

    first commit

---
+++ Tirsdag/JPARelations/target/generated-sources/annotations/entity/Customer_.java
@@ -0,0 +1,17 @@
+package entity;
+
+import java.util.List;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-09-17T07:32:05")
+@StaticMetamodel(Customer.class)
+public class Customer_ { 
+
+    public static volatile SingularAttribute<Customer, String> firstName;
+    public static volatile SingularAttribute<Customer, String> lastName;
+    public static volatile SingularAttribute<Customer, List> hobbies;
+    public static volatile SingularAttribute<Customer, Long> id;
+
+}
